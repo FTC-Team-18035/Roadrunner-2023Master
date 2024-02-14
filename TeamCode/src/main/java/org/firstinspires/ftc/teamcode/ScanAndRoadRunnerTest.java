c76b55541367608e620949d9d238fd59806f48b4
FTC18035 <ftc18035@gmail.com>
2024-02-13 19:50:05 -0500

    Added skeleton code for backdrop side autonomous. Added a master class for scan and a test file for it. When you get a chance try running it ScanAndRoadRunnerTest. Ignore the roadrunner part. It doesn't use it *PATCHED AGAIN*

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScanAndRoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ScanAndRoadRunnerTest.java
@@ -5,28 +5,30 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-@Autonomous(name = "Scan Master Class Test")
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Scan Master Class Test")
 public class ScanAndRoadRunnerTest extends LinearOpMode {
-//Testing
+    //Testing
     public void runOpMode() {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
         Scan scan = new Scan();
 
         scan.Initialize();
-        scan.telemetryTfod();
-        waitForStart();
 
-            if(scan.location == "Left") {
-               drive.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE_GREEN);
+        waitForStart();
+        while (opModeIsActive()) {
+            scan.telemetryTfod();
+            if (scan.location == "Left") {
+                drive.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE_GREEN);
                 // Actions.runBlocking(
-                 //       drive.actionBuilder(new Pose2d(0, 0, 0))
-                   //             .build());
-            }
-            else if(scan.location == "Middle"){
+                //       drive.actionBuilder(new Pose2d(0, 0, 0))
+                //             .build());
+            } else if (scan.location == "Middle") {
                 drive.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.VIOLET);
-            }
-            else if(scan.location == "Right"){
+            } else if (scan.location == "Right") {
                 drive.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
             }
         }
     }
+}
